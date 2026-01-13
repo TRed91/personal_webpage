b9fa3be19eb634da009986b0afa0497ba7ade369
TRed91 <thomas.roth@aon.at>
2026-01-13 15:36:03 +0100

    Use Hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import routes from './routes'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import {StrictMode} from "react";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
